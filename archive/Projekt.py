d6bacf6c9ebf2d1fa755fb113fdf95b617d6ef88
KarasDominik <dominik.karas99@gmail.com>
2023-05-03 12:10:48 +0100

    First part of the project done

--- archive/Projekt.py
+++ archive/Projekt.py
@@ -14,10 +14,10 @@ pd.set_option("display.max_columns", None)
 # Create DataFrame from our data
 df = pd.DataFrame(data)
 
-# Print information about our dataset including columns, datatypes and range
+# # Print information about our dataset including columns, datatypes and range
 # print(df.info())
 
-# Check if there are any values missing
+# # Check if there are any values missing
 # print(df.isnull().sum())
 
 # Replace 0 with avg
@@ -31,54 +31,81 @@ for factor in df:
     df[factor] = df[factor].fillna(avg)
 
 # Describe data
-# print(df.describe())
+#print(df.describe())
 
-# Data shape
+# # Data shape
 # print(df.shape)
 
-# Covariances
+# # Pair plot
+# sns.pairplot(data=df, hue="Outcome")
+# plt.show()
+
+# # Box plots for all variables
+# df.plot(kind ='box', subplots = True, sharey = False, figsize = (10,6))
+# plt.subplots_adjust(wspace=0.5)
+# plt.show()
+
+# # Histogram for outcome and age
+# plt.hist(df[df['Outcome']==1]['Age'], bins=5)
+# plt.title("Distribution of Age for Women who has Diabetes")
+# plt.xlabel('Age')
+# plt.ylabel('Frequency')
+# plt.show()
+
+# # Histogram for outcome and BMI
+# plt.hist(df[df['Outcome'] == 1]['BMI'], bins=5)
+# plt.title("Distribution of BMI for Women who has Diabetes")
+# plt.xlabel('Body Mass Index')
+# plt.ylabel('Frequency')
+# plt.show()
+
+# # Covariances
 # print(df.cov())
 
-# Correlations
+# # Correlations
 # print(df.corr())
 
-# Create a heat map
+# # Create a heat map
 # plt.figure(figsize = (12, 10))
 # sns.heatmap(df.corr(), annot=True)
 # plt.show()
 
 # Graphs
 
-# Pregnancy by age
+# # Pregnancy by age
 # plt.scatter(df['Age'], df['Pregnancies'], color = "black", marker = "x")
 # plt.xlabel("Age")
 # plt.ylabel("Pregnancies")
 # plt.title("Pregnancies by age")
 # plt.figure()
 
-# Outcome by glucose
-# df.boxplot(column='Glucose', by='Outcome')
+# # Insulin by glucose
+# sns.scatterplot(x="Glucose", y = "Insulin", data = df)
 # plt.show()
 
-
-# Skin thickness by BMI
+# # Skin thickness by BMI
 # plt.scatter(df['SkinThickness'], df['BMI'], color="magenta")
 # plt.xlabel("SkinThickness")
 # plt.ylabel("BMI")
 # plt.title("Skin thickness by BMI")
 # plt.show()
 
+# # Glucose by outcome
+# sns.boxplot(data=df, x='Outcome', y='Glucose')
+# plt.title("Glucose level by outcome")
+# plt.show()
+
 # Transformation
 
 # Normalisation
-scaler = MinMaxScaler()
-norm = scaler.fit_transform(df[['Insulin']].values)
-plt.plot(df['Insulin'])
-plt.title("NON Normalised")
-plt.figure()
-plt.plot(norm)
-plt.title("Normalised")
-plt.show()
+# scaler = MinMaxScaler()
+# norm = scaler.fit_transform(df[['Insulin']].values)
+# plt.plot(df['Insulin'])
+# plt.title("NON Normalised")
+# plt.figure()
+# plt.plot(norm)
+# plt.title("Normalised")
+# plt.show()
 
 # Standardization
 # scale = StandardScaler()
